55d4ddd836b5223990d29c2f2a8b56be42bce516
Leonhard Richter <leonhard.richter@outlook.de>
2022-08-08 14:17:38 +0200

    finish gram_matrix, find strange bug with delta

--- tdvp.py
+++ tdvp.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy import pi
 from numpy.typing import NDArray, ArrayLike, DTypeLike
 
-from itertools import combinations
+from itertools import combinations, combinations_with_replacement
 from typing import Callable, Iterable
 
 import scipy as sp
@@ -17,9 +17,6 @@ import plotly.express as px
 from qutip import *
 from qutip.qip.operations import *
 from qutip.qip.circuit import QubitCircuit, Gate
-
-from qaoa import qaoa_circuit
-
 #%%
 
 def sz(n:int,i:int):
@@ -84,12 +81,22 @@ class tdvp_result():
 
 
 class tdvp_optimizer():
-    def __init__(self, psi:Callable[[tuple[float]],Qobj], H:Qobj, qubo:NDArray) -> None:
+    def __init__(self, psi:Callable[[tuple[float]],Qobj], H:Qobj, qubo:NDArray, gram_mode:str='double') -> None:
         self._psi = psi 
         self._H = H
         assert qubo.T.all()==qubo.all(), "Qubo matrix not symmetric" 
         self._qubo = qubo
         self.n = len(H.dims[0])
+        assert gram_mode in {'single', 'double'}
+        self._gram_mode = gram_mode
+
+    @property
+    def gram_mode(self):
+        """The gram_mode property."""
+        return self._gram_mode
+    @gram_mode.setter
+    def gram_mode(self, value):
+        self._gram_mode = value
 
     @property
     def qubo(self):
@@ -138,17 +145,17 @@ class tdvp_optimizer():
         """
         pass
 
-    def get_qaoa_gram(self,pars:dict):
+    def get_qaoa_gram(self,delta:tuple)->NDArray:
         """evaluate the gram matrix on a quantum circuit.
 
         Args:
-            pars (dict): dict with 'gammas':tuple and 'betas':tuple
+            delta (tuple): tuple of qaoa parameters. In the first half are betas, in the second one gammas
 
         Returns:
-            _type_: _description_
+            NDArray: Gram-matrix
         """
-        p = int(len(pars)/2)
-        def U_i(pars:dict,oper:Gate,i:int):
+        p = int(len(delta)/2)
+        def U_i(delta:tuple,opers:list[Gate],i:int, tilde:bool=False):
             if self.qubo is not None:
                 # define what to apply to the circuit in each H_p turn
                 def qcH(gamma:float) -> QubitCircuit:
@@ -176,61 +183,124 @@ class tdvp_optimizer():
 
 
             qc = QubitCircuit(self.n)
-            for _ in range(p):
-                qc.add_circuit(qcH(pars['gammas'][i]))
-                qc.add_circuit(qcB(pars['betas'][i]))
-                if _==i: qc.add_gate(oper)
-            
-            return qc
-
-        def U_i_tilde(pars:dict,oper:Gate,i:int):
-            if self.qubo is not None:
-                # define what to apply to the circuit in each H_p turn
-                def qcH(gamma:float) -> QubitCircuit:
-                    qc = QubitCircuit(self.n)
-                    qc.user_gates = {"RZZ",rzz}
-                    for j in range(self.n):
-                        qc.add_gate("RZ",targets=j, arg_value=2*gamma*self.qubo[j][j],arg_label=f"2*{round(gamma,2)}*{self.qubo[j][j]}")
-                    for j,k in combinations(range(self.n),2):
-                        qc.add_gate("RZZ", targets=[j,k], arg_value=2*gamma*self.qubo[j][k],arg_label=f"2*{round(gamma,2)}*{self.qubo[j][k]}")
-                    return qc
+            if tilde:
+                for _ in range(p):
+                    qc.add_circuit(qcH(delta[i+p]))
+                    if _==i:
+                        for oper in opers: qc.add_gate(oper)
+                    qc.add_circuit(qcB(delta[i]))
             else:
-                assert self.H.isherm
-                def qcH(gamma:float) -> QubitCircuit:
-                    def H_exp(arg_value):
-                        return (-1j*arg_value*self.H).expm()
-                    qc = QubitCircuit(self.n)
-                    qc.user_gates = {"H_exp":H_exp}
-                    qc.add_gate("H_exp", arg_value=gamma,arg_label=f"{round(gamma,2)}")
-                    return qc
-
-            def qcB(beta:float) -> QubitCircuit:
-                qc = QubitCircuit(self.n)
-                qc.add_1q_gate("RX",arg_value=2*beta)
-                return qc
-
-            def oper_gate():
-                return oper
-
-            qc = QubitCircuit(self.n)
-            for _ in range(p):
-                qc.add_circuit(qcH(pars['gammas'][i]))
-                if _==i: qc.add_gate(oper)
-                qc.add_circuit(qcB(pars['betas'][i]))
-            
+                for _ in range(p):
+                    print(delta)
+                    qc.add_circuit(qcH(delta[i+p]))
+                    qc.add_circuit(qcB(delta[i]))
+                    if _==i: 
+                        for oper in opers: qc.add_gate(oper)
             return qc
 
-        # for the adjont circuits use U_i(-pars,oper=RX(-pi/2)).reverse() and so on
+        # two methods: one for computing both sides sepeartely (2 circuits) and for one longer circuit
 
-        return U_i
+        if self.gram_mode == 'single':
+            def A(left:tuple, right:tuple, delta):
+                """compute one summand of G_ij
+
+                Args:
+                    left  (tuple): (H1:[Gate],i:int,tilde:bool)
+                    right (tuple): (H2:[Gate],j:int,tilde:bool)
+                """
+                right_state = U_i(delta,*right).run(tensor([minus for _ in range(self.n)]))
+                left_state = dag(U_i(delta,*left).run(tensor([minus for _ in range(self.n)])))
+                return left_state*right_state
+
+            
+        if self.gram_mode == 'double':
+            def A(left:tuple, right:tuple, delta):
+                """compute one summand of G_ij
+
+                Args:
+                    left  (tuple): (H1:[Gate],i:int,tilde:bool)
+                    right (tuple): (H2:[Gate],j:int,tilde:bool)
+                """
+                m_delta = (-t for t in delta)
+                qc = QubitCircuit(self.n)
+                qc.add_circuit(U_i(delta,*right))
+                leftqc = U_i(m_delta,*left)
+                leftqc.add_1q_gate("H")
+                qc.add_circuit(leftqc.reverse_circuit())
+                return qc.run(tensor([minus for _ in range(self.n)]))[0]
+                
+                
+
+        G = np.zeros((p,p))
+        for i,j in combinations_with_replacement(range(p),2):
+            print((i,j))
+
+            if i <= p and j <= p:
+                G[i,j] = sum([
+                    A(
+                        ([Gate("X",[k])],i,False),([Gate("X",[l])],j,False)
+                        , delta)
+                    for k,l in combinations_with_replacement(range(self.n),2)
+                ])
+
+            if i <= p and j > p:
+                G[i,j] = sum([
+                    self.qubo[l][l]*A(
+                        ([Gate("X",[k])],i,False),([Gate("Z",[l])],j,True)
+                        , delta)
+                    for k,l in combinations_with_replacement(range(self.n),2)
+                ]) + sum([
+                    2*self.qubo[l][m]*A(
+                        ([Gate("X",[k])],i,False),([Gate("Z",[l]),Gate("Z",[m])],j,True)
+                        , delta)  # 2* is due to qubo being symmetric and not upper triangular
+                    for k,l,m in combinations_with_replacement(range(self.n),3) if l<m
+                ])
+
+            if i > p and j <= p:
+                G[i,j] = sum([
+                    self.qubo[k][k]*A(
+                        ([Gate("Z",[k])],i,True),([Gate("X",[l])],j,False)
+                        , delta)
+                    for k,l in combinations_with_replacement(range(self.n),2)
+                ]) + sum([
+                    2*self.qubo[k][l]*A(
+                        ([Gate("Z",[k]),Gate("Z",[l])],i,True),([Gate("X",[m])],j,False)
+                        , delta)
+                    for k,l,m in combinations_with_replacement(range(self.n),3) if k<l
+                ])
+
+            if i > p and j > p:
+                G[i,j] = sum([
+                    self.qubo[k,k]*self.qubo[l,l]*A(
+                        ([Gate("Z",[k])],i,True),([Gate("Z",[l])],j,True)
+                        , delta)
+                    for k,l in combinations_with_replacement(range(self.n),2)
+                ]) + sum([
+                    2*self.qubo[k,l]*self.qubo[m,m]*A(
+                        ([Gate("Z",[k]),Gate("Z",l)],i,True),([Gate("Z",[m])],j,True)
+                    , delta)
+                    for k,l,m in combinations_with_replacement(range(self.n),3) if k<l
+                ]) + sum([
+                    self.qubo[k,k]*2*self.qubo[l,m]*A(
+                        ([Gate("Z",[k])],i,True),([Gate("Z",[l]),Gate("Z",[m])],j,True)
+                    , delta)
+                    for k,l,m in combinations_with_replacement(range(self.n),3)
+                ]) + sum([
+                    2*self.qubo[k,l]*2*self.qubo[m,n]*A(
+                        ([Gate("Z",[k]),Gate("Z",[l])],i,True),([Gate("Z",[m]),Gate("Z",[n])],j,True)
+                    , delta)
+                    for k,l,m,n in combinations_with_replacement(range(self.n),4) if k<l and m<n
+                ])
+        return np.matrix(G)
 
 # %%
 def psi(pars:tuple[float])->Qobj:
     return tensor([np.cos(pars[0]/2)*basis(2,0)+np.exp(1j*pars[1])*np.sin(pars[0]/2)*basis(2,1) for _ in range(2)])
             
 tdvp_ = tdvp_optimizer(psi,tensor(sigmaz(),sigmaz()),np.array([[1,2],[2,1]]))
-U_i = tdvp_.get_qaoa_gram({"betas":(1,1),"gammas":(1,1)})
-U_i(pars={"betas":(1,1,1,1),"gammas":(1,1,1,1)},oper=Gate("X",[1]),i=0).reverse_circuit()
+U_i = tdvp_.get_qaoa_gram((1,1,1,1))
+
 # %
+
 
 # %%
