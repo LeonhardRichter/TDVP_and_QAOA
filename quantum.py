2dc45878b5d600327cc74693e62ba5f189a7915b
Leonhard Richter (Git Kraken) <leonhard.richter@outlook.de>
2022-10-14 19:52:32 +0200

    implement cheat variant of metrix and gradient

--- quantum.py
+++ quantum.py
@@ -1,4 +1,4 @@
-from itertools import permutations
+from itertools import combinations, permutations
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
 from qutip import sigmaz, sigmax, basis, tensor, qeye, Qobj
@@ -7,6 +7,11 @@ from qutip.qip.operations import expand_operator, rz
 
 def H_exp(arg_value, H) -> Qobj:
     return (-1j * arg_value * H).expm()
+
+
+# a 'cheat' gate just giving back the input. Used for the efficient simulation of the metric
+def cheat_gate(H: Qobj) -> Qobj:
+    return H
 
 
 def sz(n: int, i: int) -> Qobj:
@@ -56,6 +61,6 @@ def H_from_qubo(qubo: ArrayLike, constant: float = None) -> Qobj:
         (-1 / 2) * sum((qubo[i][i] + qj[i]) * sz(n, i) for i in range(n))
         + (1 / 4)
         * sum(qubo[j][k] * sz(n, j) * sz(n, k) for j, k in permutations(range(n), 2))
-        + qconstant
+        # + qconstant
     )
     return H
